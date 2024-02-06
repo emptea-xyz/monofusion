05c25ce2a8c036952734e797d6ef2592d2cb732e
maruthan4r <mtcreatives@hotmail.com>
2024-02-06 13:47:29 +0100

    add wallet-adapter dependencies

--- src/components/backdrop/Backdrop.js
+++ src/components/backdrop/Backdrop.js
@@ -1,8 +1,12 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+
+
+import CreateSingleAttributeAdder from "./createSingleAttributeAdder/Modal";
+import WalletConnector from "./walletConnector/Modal";
+import NetworkManager from "./networkManager/Modal";
+import ThemeSelector from "./themeSelector/Modal";
 import "./backdrop.css";
 
 /* 
@@ -11,7 +15,7 @@ BACKDROP GUIDE
 1 = WALLET CONNECTOR
 2 = NETWORK MANAGER
 3 = DESIGN SWITCH
-4 = create-single-panel: ATTRIBUTE EDITOR-
+4 = create-single-panel: ATTRIBUTE ADDER
 */
 
 /**
@@ -22,18 +26,6 @@ BACKDROP GUIDE
  */
 
 export default function Backdrop({ modal, setModal, resetModal }) {
-  //hook to control the state of the network-switch, from modal "network-manager"
-  const [switchModal, setSwitchModal] = useState(false);
-
-  //function to toggle between the boolean state of "switchModal"
-  //executed whenever you  click on the network-switch in "network-manager"
-  const toggleSwitch = () => {
-    if (switchModal != true) {
-      setSwitchModal(true);
-    } else {
-      setSwitchModal(false);
-    }
-  };
   return (
     <AnimatePresence>
       {modal != 0 && (
@@ -58,78 +50,33 @@ export default function Backdrop({ modal, setModal, resetModal }) {
               e.stopPropagation();
             }}
           >
-            {modal == 4 && (
-              <>
-                <div className="create-attributes">
-                  <div className="input">
-                    <input type="text" className="key" placeholder="key" />
-                    <input type="text" className="value" placeholder="value" />
-                  </div>
-                  <div className="submit">add</div>
-                </div>
-              </>
-            )}
             {modal == 1 && (
-              <>
-                <div className="navbar-connector">
-                  <div className="icon phantom"></div>
-                  <div className="icon solflare"></div>
-                  <div className="icon backpack"></div>
-                </div>
-              </>
+              <WalletConnector
+                modal={modal}
+                setModal={setModal}
+                resetModal={resetModal}
+              />
             )}
             {modal == 2 && (
-              <>
-                <div className="navbar-network-manager">
-                  <div className="default">
-                    <div className="info">use the default network</div>
-                    <motion.div
-                      className={
-                        switchModal == true
-                          ? "switch active"
-                          : "switch inactive"
-                      }
-                      onClick={toggleSwitch}
-                    >
-                      <motion.div className="base">
-                        <motion.div className="handle"></motion.div>
-                      </motion.div>
-                    </motion.div>
-                  </div>
-                  <div className="line"></div>
-                  <div className="custom">
-                    <div className="info">
-                      use your own RPC for full control. <br />
-                      Don't have one? Get started for free at{" "}
-                      <Link href={"https://helius.dev"} target="blank">
-                        Helius.
-                      </Link>
-                    </div>
-                    <div className="form">
-                      <input type="text" placeholder="enter RPC URL" />
-                      <motion.div className="submit" onClick={resetModal}>
-                        Go
-                      </motion.div>
-                    </div>
-                  </div>
-                </div>
-              </>
+              <NetworkManager
+                modal={modal}
+                setModal={setModal}
+                resetModal={resetModal}
+              />
             )}
             {modal == 3 && (
-              <>
-                <div className="navbar-themify">
-                  <motion.div
-                    className={
-                      switchModal == true ? "switch active" : "switch inactive"
-                    }
-                    onClick={toggleSwitch}
-                  >
-                    <motion.div className="base">
-                      <motion.div className="handle"></motion.div>
-                    </motion.div>
-                  </motion.div>
-                </div>
-              </>
+              <ThemeSelector
+                modal={modal}
+                setModal={setModal}
+                resetModal={resetModal}
+              />
+            )}
+            {modal == 4 && (
+              <CreateSingleAttributeAdder
+                modal={modal}
+                setModal={setModal}
+                resetModal={resetModal}
+              />
             )}
           </motion.div>
         </motion.div>
