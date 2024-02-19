eff3189b089b0cfeae250df437631615ab38b82f
Maru <71565617+them2dt@users.noreply.github.com>
2024-02-19 13:41:09 +0000

    add pseudo-code

--- src/backend/Single.js
+++ src/backend/Single.js
@@ -9,11 +9,73 @@ import {
 import { clusterApiUrl } from "@solana/web3.js";
 
 /**
- * Creates a new NFT (Non-Fungible Token).
+ * Creates a new Metaplex Standard NFT (Non-Fungible Token).
  * @returns {Promise<string>} The signature of the transaction.
  */
 export async function createStandardNFT() {
-  // Connect to the cluster.
-  const connection = new Connection(clusterApiUrl("devnet"));
-  const umi = createUmi(connection);
+  //Connect to the cluster.
+
+  //create an umi instance
+
+  //configure umi
+
+  //upload image
+
+  //validate inputs
+
+  //upload metadata
+
+  //create account
+
+  //mint account
+
+  //return transaction & mint address
+}
+
+/**
+ * Creates a new compressed NFT (Non-Fungible Token).
+ * @returns {Promise<string>} The signature of the transaction.
+ */
+export async function createCompressedNFT() {
+  //Connect to the cluster.
+
+  //create an umi instance
+
+  //configure umi
+
+  //upload image
+
+  //validate inputs
+
+  //upload metadata
+
+  //create account
+
+  //mint account
+
+  //return transaction & mint address
+}
+
+/**
+ * Creates a new programmable NFT (Non-Fungible Token).
+ * @returns {Promise<string>} The signature of the transaction.
+ */
+export async function createProgrammableNFT() {
+  //Connect to the cluster.
+
+  //create an umi instance
+
+  //configure umi
+
+  //upload image
+
+  //validate inputs
+
+  //upload metadata
+
+  //create account
+
+  //mint account
+
+  //return transaction & mint address
 }
